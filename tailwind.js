58a32f76b21e0929e074fb7f08e137ac736a4188
Your Name <you@example.com>
2024-06-04 21:32:19 +0100

    premiere sauvegarde

---
+++ tailwind.js
@@ -0,0 +1,35 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                'orange-900': '#401408',
+                'green-900': '#275738',
+                'amber-800': '#2B0A01',
+                'amber-100': '#F6DEE3', 
+                'gray-100': '#F8F8F8',
+                'gray-200': '#D0D0D0',
+                'gray-400': '#ECECEC',
+                'gray-600': '#7B7B7B',
+                'gray-700': '#686A79',
+                'gray-800': '#5C5C5C',
+                'dark-300': '#191A23',
+            },
+            
+            padding: {
+                '36': '8.75rem', 
+            },
+
+            fontSize: {
+                '3xl' : '1.75rem',
+            },
+
+            boxShadow: {
+                'form': '0px 4px 32px 0px #FFFFFF0A',
+                'img': '0.94px solid #FFFFFF3B',
+            },
+        }
+    },
+
+    plugins: [],
+}
